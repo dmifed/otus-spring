13eb423d0cabcd9fafbf70b8cb191b6db1865a55
dmifed <dmitriy_fedorov@list.ru>
2022-04-29 17:26:34 +0300

    exs 1 done!

--- spring-01/spring-01-exercise/src/main/java/edu/spring/Main.java
+++ spring-01/spring-01-exercise/src/main/java/edu/spring/Main.java
@@ -2,16 +2,23 @@ package edu.spring;
 
 //import org.springframework.context.support.ClassPathXmlApplicationContext;
 import edu.spring.domain.Person;
+import edu.spring.service.PersonService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+
+        PersonService personService = context.getBean(PersonService.class);
         // TODO: создайте здесь класс контекста
 
         // TODO: Получите Person Service
 
         // Получите Person "Ivan"
-        Person ivan = null;
+        Person ivan = personService.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
     }
 }
